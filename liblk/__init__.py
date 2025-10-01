96ccd9e0167fe08bea5e0fcfa5b33d0a4568cb52
R0rt1z2 <me@r0rt1z2.com>
2025-10-01 11:53:44 +0200

    liblk: Bump version to 3.0.2

--- liblk/__init__.py
+++ liblk/__init__.py
@@ -3,7 +3,7 @@ SPDX-FileCopyrightText: 2025 Roger Ortiz <me@r0rt1z2.com>
 SPDX-License-Identifier: GPL-3.0-or-later
 """
 
-__version__ = '3.0.1'
+__version__ = '3.0.2'
 
 from liblk.constants import AddressingMode, Magic
 from liblk.exceptions import InvalidLkPartition, NeedleNotFoundException
